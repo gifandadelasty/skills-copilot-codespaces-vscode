81717124a9dcb871059ddddba95e7e09c886cc4a
Gifanda Delasty <47444937+gifandadelasty@users.noreply.github.com>
2025-02-01 11:26:54 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,32 +1,35 @@
 // create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+// create a web server
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-app.use(bodyParser.json());
+// create server
+http.createServer((req, res) => {
+    // parse the url
+    const parsedUrl = url.parse(req.url, true);
 
-// create comments array
-var comments = [
-    { "id": 1, "author": "Morgan McCircuit", "text": "Great picture!" },
-    { "id": 2, "author": "Bending Bender", "text": "Excellent stuff" }
-];
+    // extract the comments
+    if (parsedUrl.pathname === '/comments') {
+        // read the comments from the file
+        fs.readFile('comments.json', (err, data) => {
+            if (err) {
+                // send an error response
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'An error occurred' }));
+                return;
+            }
 
-// create a function to get the comments
-function getComments() {
-    return comments;
-}
-
-// create a function to add a comment
-function addComment(comment) {
-    comments.push(comment);
-}
-
-// create a route to get the comments
-app.get('/comments', function (req, res) {
-    res.json(getComments());
-});
-
-// create a route to add a comment
-app.post('/comments', function (req, res) {
-    var comment = {
-        id: comments.length + 1,
+            // send the comments as a JSON array
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(data);
+        });
+    } else {
+        // send a 404 response
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Not found' }));
+    }
+}).listen(3000);
+// Output:
+// When you run the server and navigate to http://localhost:3000/comments, you should see the comments that were saved in the comments.json file.
+// If you navigate to any other URL, you should see a 404 error.
